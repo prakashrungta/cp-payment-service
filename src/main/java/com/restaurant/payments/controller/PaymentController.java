4a08ad0886caeff215f93965ebc873e8f97a15bc
Prakash Rungta <contact.prakash.rungta@gmail.com>
2025-04-05 18:18:00 +0530

    Added RareLimiter

--- src/main/java/com/restaurant/payments/controller/PaymentController.java
+++ src/main/java/com/restaurant/payments/controller/PaymentController.java
@@ -4,6 +4,7 @@ import com.restaurant.payments.dto.PaymentRequest;
 import com.restaurant.payments.entity.Payment;
 import com.restaurant.payments.service.PaymentService;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -11,6 +12,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -33,11 +35,16 @@ public class PaymentController {
         @ApiResponse(responseCode = "403", description = "Forbidden"),
         @ApiResponse(responseCode = "500", description = "Internal server error")
 })
+
+@RateLimiter(name = "paymentRateLimiter", fallbackMethod = "rateLimitFallback")
     public ResponseEntity<Payment> makePayment(
             @PathVariable Integer orderId, @Valid  @RequestBody PaymentRequest request) {
         return ResponseEntity.ok(paymentService.makePayment(orderId, request));
 
     }
 
-
+    public ResponseEntity<String> rateLimitFallback(Integer orderId, PaymentRequest request, Throwable t) {
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                .body("Rate limit exceeded. Please try again later.");
+    }
 }
